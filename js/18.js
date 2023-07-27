7a93c2f96a951b8f96659cd53db467e2fc41b07d
ChemaJob <chema.job.24@gmail.com>
2023-07-27 16:17:04 -0600

    Primer comit para subir carpteta

---
+++ js/18.js
@@ -0,0 +1,13 @@
+
+// Scope de Variables
+const precio = 300;
+
+function unaFuncion() {
+    const precio = 400;
+    console.log(precio)
+}
+unaFuncion()
+
+if( true ) {
+    console.log(precio)
+}

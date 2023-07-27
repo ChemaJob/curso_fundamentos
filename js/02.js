6e07bd5562ff36f673ca059d8687869db2130b10
ChemaJob <chema.job.24@gmail.com>
2023-07-27 17:28:39 -0600

    Creacion de 02.js

---
+++ js/02.js
@@ -0,0 +1,17 @@
+//Variables const
+
+const cliente = "Chema"
+const precioDescuento = 220 
+
+//No se puede reasignar
+
+//cliente = 20
+
+//No pueden inicar sin valor
+
+//const precio
+
+//console.log(precio)
+
+console.log(cliente)
+console.log(precioDescuento)

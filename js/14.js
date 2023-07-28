4aa1d016f6a4c9f6bebee876b4b81cdefcddae27
ChemaJob <chema.job.24@gmail.com>
2023-07-28 12:35:11 -0600

    Creacion de 14.js e ida al baño

---
+++ js/14.js
@@ -0,0 +1,15 @@
+//Funciones - Arrow Functions
+// const sumar = (numero = 0, numero2 = 0) => {
+//   return numero + numero2
+// }
+
+// const sumar = (numero = 0, numero2 = 0) => numero + numero2//Solo funciona si hay una linea de codigo, de lo contrario debe agrgarse llaves como arriba
+// const resultado = sumar(20, 30)
+
+// const sumar = numero => numero + 20 //Solo si tiene un parámetro
+// const resultado = sumar(30)
+
+const sumaArrow = () => 2 + 2 
+const resultado = sumaArrow()
+
+console.log(resultado)

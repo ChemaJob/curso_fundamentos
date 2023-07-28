fc16d80fec0d23929a64700c322513f598d77795
ChemaJob <chema.job.24@gmail.com>
2023-07-28 15:26:24 -0600

    Seccion 2, cuanto JavaScript debo saber antes de React - Selectores del DOM. querySelectorAll

--- js/22.js
+++ js/22.js
@@ -2,12 +2,15 @@
 
 const heading = document.querySelector('.heading')
 
-console.log(heading)
-console.log(heading.textContent)
-console.log(heading.tagName)
-console.log(heading.classList)
-console.log(heading.id)
+// console.log(heading)
+// console.log(heading.textContent)
+// console.log(heading.tagName)
+// console.log(heading.classList)
+// console.log(heading.id)
 
 const enlaces = document.querySelectorAll('.navegacion a')
 
+const noExiste = document.querySelectorAll('.no-existe')
+
 console.log(enlaces)
+console.log(noExiste)

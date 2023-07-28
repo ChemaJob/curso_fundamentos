378097ed06cc331597ed6467259db2846e646447
ChemaJob <chema.job.24@gmail.com>
2023-07-27 18:02:14 -0600

    Creacion de 03.js

---
+++ js/03.js
@@ -0,0 +1,55 @@
+//Tipos de datos
+
+//Undefined
+//let cliente
+//console.log(cliente)
+//console.log(tipeof cliente)
+
+//Boolean
+//const descuento = false
+//console.log(descuento)
+//console.log(typeof descuento)
+
+//Number
+// const numero1 = 20.20
+// const numero2 = 30
+// const numero3 = -100
+
+// console.log(typeof numero1)
+// console.log(typeof numero2)
+// console.log(typeof numero3)
+
+//Strings o cadenas de texto
+// const alumno = 'Chema'
+// let producto = 'Monitor 20 pulgadas'
+// let producto2 = 'Monitor 20 pulgadas'
+
+// const numero = "30"
+// const numero2 = 30
+
+// console.log(typeof numero)
+// console.log(typeof numero2)
+
+//Big int
+
+// const numeroGrande = BigInt(1234567899876543210123456789)
+
+// console.log(typeof numeroGrande)
+
+// const numero = 10
+// const numero2 = 20
+
+// console.log(numero + Number(numeroGrande))
+
+//Symbol
+// const primerSymbol = Symbol(30)
+// const segundoSymbol = Symbol(30)
+
+// console.log(primerSymbol === segundoSymbol)
+// console.log(primerSymbol.valueOf())
+// console.log(segundoSymbol.valueOf())
+
+//Null
+
+const descuento = null
+console.log(typeof descuento)

a01e6751644c0789cc9fb33b0c4665fa3b21b91e
ChemaJob <chema.job.24@gmail.com>
2023-07-28 15:39:28 -0600

    Seccion 2, cuanto JavaScript debo saber antes de React - Eventos del DOM. Clicks

---
+++ js/24.js
@@ -0,0 +1,21 @@
+// Eventos del DOM - Clicks
+
+const heading = document.querySelector('.heading')
+
+// heading.addEventListener('click', function() {
+//   console.log('Diste click en heading')
+// })
+
+heading.addEventListener('click', () =>  {
+    heading.textContent = 'Nuevo Heading al dar click'
+})
+
+
+
+const enlaces = document.querySelectorAll('.navegacion a')
+
+enlaces.forEach( enlace => {
+    enlace.addEventListener('click', () => {
+        console.log('Diste click en un enlace')
+    })
+})

4854ef2318a4a07bea4fcb98b79dbbaf85a7290a
ChemaJob <chema.job.24@gmail.com>
2023-07-28 11:29:11 -0600

    Creacion de 11.js

---
+++ js/11.js
@@ -0,0 +1,18 @@
+//Iteradores en JS
+
+const tecnologias = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+
+// forEach - Acceder a cada elemento del array
+
+const arrayForEach = tecnologias.forEach( function(tech){
+  return tech
+})
+
+// map - Crea un nuevo Array
+
+const arrayMap = tecnologias.map( function(tech){
+  return tech
+})
+
+console.log(arrayForEach)
+console.log(arrayMap)

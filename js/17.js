77686d5e2fa2bfabb419a6061d28cff493ee94ac
ChemaJob <chema.job.24@gmail.com>
2023-07-28 13:36:21 -0600

    Seccion 2, cuanto JavaScript debo saber antes de React - Condicionales if. Se crea 17.js

---
+++ js/17.js
@@ -0,0 +1,17 @@
+// Condicionales
+
+const disponible = 6000
+const retirar = 7500
+
+if( disponible > retirar ) {
+    console.log('Si puedes retirar') // Si se cumple la condición se ejecuta el código
+} else {
+    console.log('Lo siento, no puedes retirar') // Si no se cumple la condición, ejecutar este código
+}
+
+
+// > - Mayor Que
+// < - Menor que
+// >=  Mayor o igual
+// <= Menor o igual
+

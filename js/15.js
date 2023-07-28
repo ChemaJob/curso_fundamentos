2b985592b16a3f15e847af91174d7509c1c04bd1
ChemaJob <chema.job.24@gmail.com>
2023-07-28 13:01:23 -0600

    Creacion de 15.js

---
+++ js/15.js
@@ -0,0 +1,16 @@
+//
+
+const tecnologias = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+
+// const nuevoArray = tecnologias.map( tech => {
+//   if (tech === 'HTML') {
+//     return 'GraphQL'
+//   } else {
+//     return tech
+//   }
+// })
+
+const nuevoArray2 = tecnologias.filter( tech => tech !== 'HTML' )
+
+// console.table(nuevoArray)
+console.table(nuevoArray2)

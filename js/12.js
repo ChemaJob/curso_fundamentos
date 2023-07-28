6f1b9a7fb326473ee55e2d1dfbd432d7a1aeefd5
ChemaJob <chema.job.24@gmail.com>
2023-07-28 11:51:46 -0600

    Se modifica 12.js

--- js/12.js
+++ js/12.js
@@ -1,7 +1,12 @@
 // Funciones - Function declaration
 
-function sumar() {
-  console.log(2 + 2)
+function sumar(numero = 0, numero2 = 0) {
+  // console.log(numero)
+  // console.log(numero2)
+  console.log(numero + numero2)
 }
 
+sumar(10, 20)
+sumar(2, 3)
+sumar(100)
 sumar()

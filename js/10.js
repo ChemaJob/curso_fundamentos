468fad97caadab657d18ce085885e6af674a0b78
ChemaJob <chema.job.24@gmail.com>
2023-07-28 11:19:18 -0600

    Creacion de 10.js

---
+++ js/10.js
@@ -0,0 +1,23 @@
+//Destructuring de Arrays
+
+const tecnologias = ["HTML", "CSS", "JavaScript", "React", "Node.js"];
+
+// const [var1, var2, var3, var4, var5] = tecnologias
+
+// console.log(var1)
+// console.log(var2)
+// // console.log(var3)
+// console.log(var4)
+// console.log(var5)
+
+// const cliente ={
+//   nombre: 'Chema',
+//   tipo: 'premium'
+// }
+
+// const {tipo} = cliente
+// console.log(tipo)
+
+const [, , , , var5] = tecnologias
+
+console.log(var5)

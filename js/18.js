13668703e76ff1f6775b1ef867c6fa7244cba7b1
ChemaJob <chema.job.24@gmail.com>
2023-07-28 13:41:41 -0600

    Seccion 2, cuanto JavaScript debo saber antes de React - Condicionales - Comparación con == y ===. Crea 18.js

---
+++ js/18.js
@@ -0,0 +1,22 @@
+// Comparación y Operador Estricto
+
+const numero1 = 20
+const numero2 = "20"
+
+// ==  ( Comparación pero no es estricto)
+// === (Comparación estricta - Revisa valor y tipo de dato)
+
+console.log(typeof numero1)
+console.log(typeof numero2)
+
+if(numero1 === numero2 ) {
+    console.log('Si, son iguales')
+} else {
+    console.log('No, no son iguales')
+}
+
+
+const autenticado = true
+if(autenticado) {
+    console.log('Si esta autenticado')
+}
